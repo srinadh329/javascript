3aa59ab96ff3151c8370dab3a51855ec09eda487
siva srinadh <srinadh329@gmail.com>
2022-01-11 22:13:09 +0530

    arrays

--- array/array.js
+++ array/array.js
@@ -65,6 +65,8 @@ let s = eveA.some(x=>{
 })
 console.log(s)
 let [ds,g] =[1,5,'ff'];
+// let [c=10,d,e=30]=[1,2,3]
+// console.log(e)
 console.log(ds,g);
 let arr1=[1,2];
 let arr2=[1,2];
@@ -106,4 +108,128 @@ console.log(as instanceof Array)
 console.log(Array.isArray(as))
 
 
-// sorts, reducer,min max , slice, toString, array keys, array.form
+// sort;
+
+//asc , desc;
+
+// asc
+let unsortedAry = [2,3,5,1,4]; 
+console.log(unsortedAry.sort((a,b)=>a-b));
+
+// des
+let unsortedAryDesc = [2,3,5,1,4]; 
+console.log(unsortedAryDesc.sort((a,b)=>b-a));
+
+// array of obj
+let unsortedAryOBj= [{id:3,name:'venki'},{id:1,name:'srinadh'},{id:2,name:'srinu'}];
+
+let res = unsortedAryOBj.sort((a,b)=>{
+    // return a.id -b.id
+    return b.id -a.id
+
+})
+console.log(res)
+
+// 
+let dsd = ['s', 't', 'a', 'c', 'K', 'o', 'v', 'E', 'r', 'f', 'l', 'W', '2', '1']
+console.log(dsd.sort((a,b)=>a.localeCompare(b)))
+
+
+
+var dates = [
+    new Date(2007, 11, 10),
+    new Date(2014, 2, 21),
+    new Date(2009, 6, 11),
+    new Date(2016, 7, 23)
+    ];
+console.log(dates.sort((a,b)=>{
+    if(a>b){
+        return -1
+    }
+    if(a<b){
+        return 1
+    }
+    return 0
+}))
+
+
+let resss = unsortedAryOBj.sort((a,b)=>{
+    // return a.id -b.id
+    if(a.id>b.id) return 1
+    if(a.id<b.id) return -1
+    return 0
+
+
+})
+console.log(resss)
+
+
+//  , array keys, array.form
+
+// redude
+let newA = [1,2,3,4,5];
+let total = newA.reduce((a,b)=>{
+    console.log(a,b)
+    return  a+b
+},0)
+console.log(total)
+// min and max
+
+let max = Math.max(...[3,4,65,34,23]);
+console.log(max)
+
+let min = Math.min(...[0,3,4,65,34,23]);
+console.log(min)
+
+let minRed = [0,3,4,65,34,23].reduce((a,b)=>{
+  return b > a ? a : b
+},0)
+console.log(minRed)
+
+let mxnRed = [0,3,4,65,34,23].reduce((a,b)=>{
+    return a > b ? a : b
+  },0)
+  console.log(mxnRed)
+
+  let mxnRedf = [0,3,4,65,34,23].reduce((a,b)=>{
+    return Math.max(a,b)
+  },0)
+  console.log(mxnRedf)
+
+  // ternary operator
+
+  let asss=10, bbb=0;
+
+    if(asss>bbb){
+        console.log(`asss is grater`)
+    }else{
+        console.log(`bbb is greater`)
+    }
+
+//
+
+let teste =(asss > bbb) ? `asss is grater` :  `bbb is greater`;
+console.log(teste)
+
+// slice
+
+let aslice = [1,2,3,4,5,5,6,6];
+let fsf = aslice.slice(0,5)
+console.log(fsf)
+console.log(aslice.slice(5))
+///toString
+
+console.log(aslice.toString())
+
+// array.form
+
+let newFormAr = Array.from([1,3,4,])
+console.log(newFormAr)
+
+// array.keys
+let myArray = [1, 2, 3, 4];
+for (let i of myArray.keys()) {
+    console.log(i)
+let twoValue = myArray[i] * 2;
+console.log("2 * value is: %d", twoValue);
+}
